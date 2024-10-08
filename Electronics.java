9862e9e000bdea8b70c2b751b7deb1a8b74279c9
Redowan Ahmed <161846618+redowannahmed@users.noreply.github.com>
2024-10-08 05:56:46 -0700

    Add files via upload

---
+++ Electronics.java
@@ -0,0 +1,15 @@
+public class Electronics extends Product {
+    private final double TAX_RATE = 0.15;
+
+    public Electronics (String name, int stockQuantity, double unitPrice)
+    {
+        super(name, stockQuantity, unitPrice);
+    }
+
+    @Override
+    public double calculateTotalPrice ()
+    {
+        return unitPrice + (unitPrice * TAX_RATE);
+    }
+
+}

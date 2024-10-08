e2542475ab83705f4d0d882ad97235a2abf2bdee
Redowan Ahmed <161846618+redowannahmed@users.noreply.github.com>
2024-10-08 05:54:34 -0700

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,30 @@
+public abstract class Product
+{
+    protected String name;
+    protected int stockQuantity;
+    protected double unitPrice;
+
+    public Product (String name, int stockQuantity, double unitPrice)
+    {
+        this.name = name;
+        this.stockQuantity = stockQuantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public abstract double calculateTotalPrice ();
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public int getStockQuantity ()
+    {
+        return stockQuantity;
+    }
+
+    public double getUnitPrice ()
+    {
+        return unitPrice;
+    }
+}
